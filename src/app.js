8231a24e4735c671cd38d772800c051e230fbed5
SNijanthan <nijanthan378@gmail.com>
2025-04-11 21:43:11 +0530

    fix: update CORS for vercel

--- src/app.js
+++ src/app.js
@@ -12,12 +12,24 @@ const { studentRouter } = require("./routes/student.router");
 
 const port = 7000;
 
+const allowedOrigins = [
+  "http://localhost:5173", // for local dev
+  "https://placement-cell-frontend-chi.vercel.app", // your deployed frontend
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173", // Whitelisting domain name
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(cookieParser());
 
